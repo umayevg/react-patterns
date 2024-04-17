6a0d13acb9095836c6a342316162da43f45b3545
umayevg <beamyg@gmail.com>
2024-04-17 16:06:16 +0200

    initial commit

---
+++ src/components/Accordion/Accordion.tsx
@@ -0,0 +1,41 @@
+import { useContext, createContext, useState } from 'react';
+import AccordionItem from './AccordionItem';
+import AccordionTitle from './AccordionTitle';
+import AccordionContent from './AccordionContent';
+
+const AccordionContext = createContext();
+
+export function useAccordionContext() {
+	const ctx = useContext(AccordionContext);
+
+	if (!ctx) {
+		throw new Error(
+			'Accordion-related components must be wrapped in an Accordion component'
+		);
+	}
+
+	return ctx;
+}
+const Accordion = ({ children, className }) => {
+	const [openItemId, setOpenItemId] = useState(null);
+
+	const toggleItem = id => {
+		setOpenItemId(prevId => (prevId === id ? null : id));
+	};
+
+	const contextValue = {
+		openItemId,
+		toggleItem,
+	};
+	return (
+		<AccordionContext.Provider value={contextValue}>
+			<ul className={className}>{children}</ul>
+		</AccordionContext.Provider>
+	);
+};
+
+export default Accordion;
+
+Accordion.Item = AccordionItem;
+Accordion.Title = AccordionTitle;
+Accordion.Content = AccordionContent;
---
+++ src/components/Accordion/AccordionContent.tsx
@@ -0,0 +1,19 @@
+import { useAccordionContext } from './Accordion';
+import { useAccordionItemContext } from './AccordionItem';
+
+const AccordionContent = ({ className, children }) => {
+	const { openItemId } = useAccordionContext();
+	const id = useAccordionItemContext();
+	const isOpen = openItemId === id;
+	return (
+		<div
+			className={
+				isOpen ? `${className ?? ''} open` : `${className ?? ''} close`
+			}
+		>
+			{children}
+		</div>
+	);
+};
+
+export default AccordionContent;
